683829d5675e56e85bda1e87c0a2fd59fdc84049
orviz <orviz@ifca.unican.es>
2023-07-10 11:48:46 +0200

    Add JePL file structure

---
+++ .sqa/script.gamy-heliotrope-sparrow.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/EOSC-synergy/sqaaas-web &&
+    cffconvert --validate
+)
--- .sqa/script.hilly-green-buffalo.sh
+++ .sqa/script.hilly-green-buffalo.sh
@@ -1,4 +1,4 @@
 (
-cd github.com/darce-i3m/sqaaas-web &&
+cd github.com/EOSC-synergy/sqaaas-web &&
     licensee detect . --confidence 60 --json
 )
