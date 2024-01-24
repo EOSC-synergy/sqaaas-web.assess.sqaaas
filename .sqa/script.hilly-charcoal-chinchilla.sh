3cb3da1cf8f10cfdd9841aef4caec65e3d67322c
orviz <orviz@ifca.unican.es>
2024-01-24 10:24:53 +0100

    Add JePL file structure

--- .sqa/script.hilly-charcoal-chinchilla.sh
+++ .sqa/script.hilly-charcoal-chinchilla.sh
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
-cd github.com/eosc-synergy/sqaaas-web &&
-    mdl --json .
+cd github.com/EOSC-synergy/sqaaas-web &&
+    git rev-parse --is-inside-work-tree
 )
