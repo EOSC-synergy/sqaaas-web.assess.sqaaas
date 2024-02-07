9d487cbff60897a20bdc5bf876e13faa64ea2a78
orviz <orviz@ifca.unican.es>
2024-02-07 12:06:15 +0100

    Add JePL file structure

--- .sqa/script.ugly-tan-seahorse.sh
+++ .sqa/script.ugly-tan-seahorse.sh
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
-cd github.com/EOSC-synergy/sqaaas-web &&
-    find_doc_files.py --file_type all
+cd github.com/eosc-synergy/sqaaas-web &&
+    git rev-parse --is-inside-work-tree
 )
