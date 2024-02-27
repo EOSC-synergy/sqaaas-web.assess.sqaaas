a0ec50bc204a0882406c523e59c9aa3aa21766e9
Pablo Orviz <orviz@ifca.unican.es>
2024-02-27 10:08:25 +0100

    Add JePL file structure

--- .sqa/script.dorky-orchid-beaver.sh
+++ .sqa/script.dorky-orchid-beaver.sh
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
-cd github.com/eosc-synergy/sqaaas-api-spec &&
-    find_doc_files.py --file_type all
+cd github.com/eosc-synergy/sqaaas-web &&
+    cffconvert --validate
 )
