75c78f18c9b2b0d3d02a91df634fca9c6c95aef7
Pablo Orviz <orviz@ifca.unican.es>
2024-04-23 10:23:24 +0200

    Add JePL file structure

--- .sqa/script.cranky-cinnabar-oyster.sh
+++ .sqa/script.cranky-cinnabar-oyster.sh
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
-cd github.com/EOSC-synergy/sqaaas-web &&
-    git rev-parse --is-inside-work-tree
+cd github.com/eosc-synergy/sqaaas-web &&
+    find_doc_files.py --file_type all
 )
