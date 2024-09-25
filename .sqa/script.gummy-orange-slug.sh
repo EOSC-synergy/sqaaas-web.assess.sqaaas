14ac5f9e77ca9ea0f837990142b83b68a1e26865
Pablo Orviz <orviz@ifca.unican.es>
2024-09-25 12:47:14 +0200

    Add JePL file structure

--- .sqa/script.gummy-orange-slug.sh
+++ .sqa/script.gummy-orange-slug.sh
@@ -5,5 +5,5 @@
 
 (
 cd github.com/eosc-synergy/sqaaas-web &&
-    mdl --json  .
+    git rev-parse --is-inside-work-tree
 )
--- .sqa/script.muzzy-ultramarine-javanese.sh
+++ .sqa/script.muzzy-ultramarine-javanese.sh
@@ -5,5 +5,5 @@
 
 (
 cd github.com/eosc-synergy/sqaaas-web &&
-    git rev-parse --is-inside-work-tree 
+    licensee detect . --confidence 60 --json
 )
